1035c61170951cc428d7d59d08c0598fc8297fe9
Alex <39405237+iamguruman@users.noreply.github.com>
2021-07-04 12:28:12 +0300

    Update _form.php

--- upload/_form.php
+++ upload/_form.php
@@ -11,10 +11,29 @@ use yii\widgets\ActiveForm;
 <div class="mkp-uploads-form">
 
     <?php $form = ActiveForm::begin(); ?>
+    
+        <?= aIfModuleControllerAction("bill_sell", "upload", "create") ?
+        $form->field($model, 'uploads[]')
+            ->fileInput(['multiple' => true])
+        //->fileInput(['multiple' => true])
+        : null ?>
+    
+        <?php if(aIfModuleControllerAction("contracts", "upload", "update")): ?>
 
-    <?= aUserMyId() == 1 ? $form->field($model, 'kp_id')->textInput() : null ?>
+            <?= $form->field($model, 'filename_original')->textInput(['maxlength' => true]) ?>
 
-    <?= aUserMyId() == 1 ? $form->field($model, 'filename_original')->textInput(['maxlength' => true]) : null ?>
+            <?= $form->field($model, 'from_scan_signed')->checkbox() ?>
+            <?= $form->field($model, 'from_original_signed')->checkbox() ?>
+            <?= $form->field($model, 'from_sended_post')->checkbox() ?>
+            <?= $form->field($model, 'from_sended_courier')->checkbox() ?>
+
+            <?= $form->field($model, 'to_scan_signed')->checkbox() ?>
+            <?= $form->field($model, 'to_original_signed')->checkbox() ?>
+            <?= $form->field($model, 'to_sended_post')->checkbox() ?>
+            <?= $form->field($model, 'to_sended_courier')->checkbox() ?>
+
+        <?php endif; ?>
+
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
