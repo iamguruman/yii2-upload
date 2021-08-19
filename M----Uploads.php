3216e08aaba8f4adbbd1a96e3456dfe886809321
Alex <39405237+iamguruman@users.noreply.github.com>
2021-08-19 15:17:52 +0300

    Update and rename MKpUploads.php to M----Uploads.php

--- M----Uploads.php
+++ M----Uploads.php
@@ -6,14 +6,12 @@ use app\modules\users\models\User;
 use Yii;
 
 /**
- * This is the model class for table "m_kp__uploads".
+ * This is the model class for table "m_----__uploads".
  *
- * @property int $id
+ * @property int $id - айди добавленного файла
  *
- * @property int $kp_id
- * @property Kp $kp
- *
- * @property int $team_id Команда по умолчанию
+ * @property int $team_by - Команда по умолчанию, используется для возможности работать с группа команд (для распределение ролей)
+ * @property Team $teamBy
  *
  * @property int $created_at  Добавлено когда
  *
@@ -33,10 +31,21 @@ use Yii;
  * @property int $isDeleted - удалить... пережиток прошлого
  *
  * @property string $filename_original Оригинальное название файла
- * @property string $md5
+ * @property string $md5 - мд5 хеш файла, чтобы проверять на дубликаты на диске не загружать дубликаты, 
+ *                         если файл является дубликатом, то в контроллере в методе actionCreate файл не загружаем, 
+ *                         но добавляем запись о том, что файл относится к объекту
  * @property string $ext Расширение файла
- * @property string $mimetype
- * @property int $size
+ * @property string $mimetype - миме тип файла
+ * @property int $size - размер файла
+ *
+ * ПОЛЕ ИДЕНТИФИКАТОР ОБЪЕКТА, к котором цепляем файл:
+ *                             
+ * @property int $object_id
+ * @property Kp $object
+ * @property Kp $kp
+ *
+ *
+ * СПИСОК ИНДИВИДУАЛЬНЫХ ПОЛЕЙ:
  * @property int $type_anketa Тип файла Анкета для нового покупателя
  *
  *
@@ -45,8 +54,8 @@ use Yii;
 DROP TABLE IF EXISTS `m_XX__uploads`;
 CREATE TABLE `m_XX__uploads` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `kp_id` int(11) DEFAULT NULL COMMENT 'Коммерческое предложение',
-  `team_id` int(11) DEFAULT NULL COMMENT 'Команда',
+  `object_id` int(11) DEFAULT NULL COMMENT 'Объект, к которому крепится файл',
+  `team_by` int(11) DEFAULT NULL COMMENT 'Команда',
   `created_at` datetime DEFAULT NULL COMMENT 'Добавлено когда',
   `created_by` int(11) DEFAULT NULL COMMENT 'Добавлено кем',
   `updated_at` datetime DEFAULT NULL COMMENT 'Изменено когда',
@@ -149,9 +158,13 @@ class MKpUploads extends \yii\db\ActiveRecord
     {
         return new MKpUploadsQuery(get_called_class());
     }
+ 
+    public function getObject(){
+        return $this->hasOne(Kp::className(), ['id' => 'object_id']);
+    }
 
     public function getKp(){
-        return $this->hasOne(Kp::className(), ['id' => 'kp_id']);
+        return $this->getkp();
     }
 
     public function getMarkdelBy(){
