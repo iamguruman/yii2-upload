1503f0434b26d691b1e4dc32aeacaa2090a17df1
Alex <39405237+iamguruman@users.noreply.github.com>
2021-08-22 12:55:36 +0300

    Update update.php

--- update.php
+++ update.php
@@ -3,12 +3,23 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-/* @var $model app\modules\kp\models\MKpUploads */
+/* @var $model \app\modules\customer_review\models\MReviewUpload */
 
-$this->title = 'Update M Kp Uploads: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'M Kp Uploads', 'url' => ['index']];
+$this->title = "Файл {$model->id}";
+
+$this->params['breadcrumbs'][] = ['label' => 'Отзывы покупателей', 'url' => ['/customer_review/']];
+
+$this->params['breadcrumbs'][] = \app\modules\kp\Module::getBreadcrumbs();
+
+if($model->object){
+    $this->params['breadcrumbs'][] = $model->object->getBreadcrumbs();
+
+    $this->params['breadcrumbs'][] = ['label' => 'Файлы',
+        'url' => ['/kp/kp/view', 'id' => $model->object->id, 'tab' => 'files']];
+}
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+
+$this->params['breadcrumbs'][] = 'Редактирование';
 ?>
 <div class="mkp-uploads-update">
 
