7c747667440460f80e5f97d608e0f43862209069
Alex <39405237+iamguruman@users.noreply.github.com>
2021-09-28 20:33:37 +0300

    Update update.php

--- update.php
+++ update.php
@@ -5,6 +5,9 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model \app\modules\customer_review\models\MReviewUpload */
 
+$module = 'route';
+$controller = 'route';
+
 $this->title = "Файл {$model->id}";
 
 $this->params['breadcrumbs'][] = ['label' => 'Отзывы покупателей', 'url' => ['/customer_review/']];
@@ -15,7 +18,7 @@ if($model->object){
     $this->params['breadcrumbs'][] = $model->object->getBreadcrumbs();
 
     $this->params['breadcrumbs'][] = ['label' => 'Файлы',
-        'url' => ['/kp/kp/view', 'id' => $model->object->id, 'tab' => 'files']];
+        'url' => ["/{$module}/{$controller}/view", 'id' => $model->object->id, 'tab' => 'files']];
 }
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 
